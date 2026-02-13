e94d180ce84028cc3386215269c0cb943ee2ba89
iamatishay <108118018+iamatishay@users.noreply.github.com>
2026-02-13 13:29:12 +0530

    Update VD.py

--- VD.py
+++ VD.py
@@ -37,7 +37,7 @@ with col3:
 # Section 2: Photo Slideshow
 st.header("Memory Lane Slideshow 📸")
 st.write("Click to flip through our favorite moments!")
-images = ["memory1.jpg", "memory2.jpg", "memory3.jpg", "memory4.jpg"]  # Add your image paths
+images = ["https://www.facebook.com/ValentinesDayImagess/", "https://www.facebook.com/ValentinesDayImagess/", "https://www.facebook.com/ValentinesDayImagess/, "https://www.facebook.com/ValentinesDayImagess/"]  # Add your image paths
 captions = [
     "That time we danced like no one was watching!",
     "Our epic picnic adventure (with extra cheese).",
